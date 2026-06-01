ce2e49256eb8b63c194284c59f9cdd44ed69d092
kabindrabohara078 <kabindrabohara1@gmail.com>
2026-06-01 08:08:10 +0530

    placeholder modified

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -23,7 +23,9 @@ const Input = (props) => {
             >
                 <div className="input-main">
                     <textarea placeholder='Enter any text' style={{
-                        backgroundColor: props.mode ? '#777' : 'white', borderRadius: '5px', border: !props.mode ? '1px solid black' : '1px solid white', color: props.mode ? 'white' : 'black', fontSize: `${fs.current}px`
+                        backgroundColor: props.mode ? '#777' : 'white', 
+                        color: props.mode ? 'white' : 'gray',
+                        borderRadius: '5px', border: !props.mode ? '1px solid black' : '1px solid white', color: props.mode ? 'white' : 'black', fontSize: `${fs.current}px`
 
                     }} className='input-box' type="text" />
                 </div>
